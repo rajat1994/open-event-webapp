c614083a90652111147441b24503a8764d2943c4
Chen Zihui <hello@chenzihui.com>
2016-02-02 16:59:26 +0800

    Remove old code

--- config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    "title": "OpenEvent",
-    "apiBaseGetUrl": "http://springboard.championswimmer.in/api/v1/",
-    "eventId": "1",
-    "use_testApi": true,
-    "themeColorPrimary": "yellow",
-    "themeColorAccent": "teal"
-};
-
