271304fdfe7b3dce3c5018cd43b9608eb33d49d2
Chen Zihui <hello@chenzihui.com>
2016-02-10 18:03:29 +0800

    Setup /sponsors to use real data

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,6 +2,6 @@ import DS  from 'ember-data';
 import ENV from 'open-event-webapp/config/environment';
 
 export default DS.JSONAPIAdapter.extend({
-  host: ENV.API_HOST,
-  namespace: ENV.API_NAMESPACE
+  host: ENV.APP.API_HOST,
+  namespace: ENV.APP.API_NAMESPACE
 });
--- app/components/sponsor-summary.js
+++ app/components/sponsor-summary.js
@@ -2,5 +2,5 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'div',
-  classNames: ['m-bottom--large', 'pad', 'one-third']
+  classNames: ['m-bottom--large', 'pad--large', 'one-third']
 });
